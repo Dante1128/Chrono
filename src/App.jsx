ccb886e2a284c4c4680118ee1781e88995f8aa8a
Dante1128 <danteyupanqui.yr@gmail.com>
2025-11-11 18:00:49 -0400

    production

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import './app.css';
+import './App.css';
 
 export default function TimeTracker() {
   const [activeTab, setActiveTab] = useState('registro');
